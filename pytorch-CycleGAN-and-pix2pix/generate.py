e1acc4f3eb424188ef121fff0e3264bd5f05747a
PlatypussDivva <platypuss@PlatypussDivvas-Mac-mini.local>
2021-08-29 13:07:34 -0700

    adding generator script

--- pytorch-CycleGAN-and-pix2pix/generate.py
+++ pytorch-CycleGAN-and-pix2pix/generate.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    opt.interval = 1 #interval off images to save
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
@@ -57,8 +58,6 @@ if __name__ == '__main__':
     if opt.eval:
         model.eval()
     for i, data in enumerate(dataset):
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
